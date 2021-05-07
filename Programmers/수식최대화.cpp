5e3074644fe83dfc06b09b3320217a72761a23ac
hello-SiOO <fltcy2039@naver.com>
2021-05-07 12:41:16 +0900

    210507 2 sol

---
+++ Programmers/수식최대화.cpp
@@ -0,0 +1,61 @@
+//
+//  수식최대화.cpp
+//  Programmers
+//
+//  Created by 임수정 on 2021/05/06.
+//
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+long long cal(long long a, long long b, char op){
+    if(op == '*'){
+        return a*b;
+    }else if(op == '+'){
+        return a+b;
+    }else{
+        return a-b;
+    }
+}
+
+long long solution(string expression) {
+    long long answer = 0;
+    vector<long long> nums;
+    vector<char> op;
+    long long num = 0;
+    for(int i = 0 ; i < expression.length() ; i++){
+        char c = expression[i];
+        if(c >= '0'&& c <= '9'){
+            num = num*10 + c-'0';
+        }else{
+            op.push_back(c);
+            nums.push_back(num);
+            num = 0;
+        }
+    }
+    nums.push_back(num);
+    
+    vector<char> opp = {'*','+','-'};
+    do{
+        vector<char> tmp_op = op;
+        vector<long long> tmp_nums = nums;
+        for(int i = 0 ; i < 3 ; i++){
+            for(int j = 0 ; j < tmp_op.size() ; j++){
+                if(tmp_op[j] == opp[i]){
+                    tmp_nums[j] = cal(tmp_nums[j],tmp_nums[j+1],tmp_op[j]);
+                    tmp_nums.erase(tmp_nums.begin()+j+1);
+                    tmp_op.erase(tmp_op.begin()+j);
+                    j--;
+                }
+            }
+        }
+        if(answer < abs(tmp_nums[0])){
+            answer = abs(tmp_nums[0]);
+        }
+    }while(next_permutation(opp.begin(), opp.end()));
+    
+    return answer;
+}
---
+++ Programmers/튜플.cpp
@@ -0,0 +1,46 @@
+//
+//  튜플.cpp
+//  Programmers
+//
+//  Created by 임수정 on 2021/05/04.
+//
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(vector<int> a, vector<int> b){
+    return a.size() < b.size();
+}
+vector<int> solution(string s) {
+    vector<int> answer;
+    vector<int> in;
+    vector<vector<int>> large;
+    int num = 0;
+    for(int i = 0 ; i < s.length() ; i++){
+        if(s[i] == '{'){
+            in.clear();
+        }else if (s[i] == '}'){
+            in.push_back(num);
+            num = 0;
+            large.push_back(in);
+            i++;
+        }else if(s[i] == ','){
+            in.push_back(num);
+            num = 0;
+        }else{
+            num = num*10 + s[i]-'0';
+        }
+    }
+    sort(large.begin(), large.end(), cmp);
+    for(int i = 0 ; i < large.size() ; i++){
+        for(int j = 0 ; j < large[i].size() ; j++){
+            int n = large[i][j];
+            if(find(answer.begin(), answer.end(), n) == answer.end()){
+                answer.push_back(n);
+            }
+        }
+    }
+    return answer;
+}
